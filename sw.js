b34b1ba6c491b532321f04566b51cae076371745
65RuudKoene <r.koene65@gmail.com>
2026-08-09 07:36:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,32 @@
-const CACHE='boodschappen-v1';
-const ASSETS=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = 'boodschappen-v2';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+// Nieuwe versie meteen installeren
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
+});
+
+// Oude caches opruimen en direct de controle overnemen
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// Network-first: altijd de nieuwste ophalen, cache als terugval (offline)
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(
+    fetch(req)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(req, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(req).then(r => r || caches.match('./index.html')))
+  );
+});
